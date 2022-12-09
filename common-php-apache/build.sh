7aebff41b076d53b44960c2e0316af8ba99d559a
Glenn Schmidt <glenn@codeacious.com>
2022-12-09 17:09:35 +1100

    Build multi-architecture images

---
+++ common-php-apache/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ "$1" == "" ]; then
+    echo "Usage: build.sh <x.y.z> (where x.y.z is the PHP version for inclusion in the image tag)" >&2
+    exit 1
+fi
+
+BUILDER=${BUILDER:-multiarch}
+IMAGE_NAME="codeacious/common-php-apache"
+PHP_VERSION="$1"
+IMAGE_VERSION="$PHP_VERSION-$(date +%Y%m%d)"
+
+
+if ! docker buildx inspect "$BUILDER" >/dev/null 2>&1
+then
+    docker buildx create --name "$BUILDER" --driver docker-container --bootstrap
+fi
+
+docker buildx build --pull "--builder=$BUILDER" "--platform=linux/amd64,linux/arm64" "--build-arg=php_version=$IMAGE_VERSION" "--tag=$IMAGE_NAME:$IMAGE_VERSION" "--tag=$IMAGE_NAME:7.4" "--tag=$IMAGE_NAME:latest" --push .
